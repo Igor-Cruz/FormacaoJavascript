4c170783d0a056a2e6cbb345745a3eb92b10903e
IGOR CRUZ <igorcruz.it@outlook.com>
2021-12-29 11:54:50 -0300

    Aula 1 aprendendo sobre: tag script e outputs

---
+++ Basic/script.js
@@ -0,0 +1,9 @@
+//alert('Hello world')
+//console.log('Hello world') - visualizar as saídas no javascript
+
+//document.write('hello world')
+
+
+//Para o javascript encontrar o id
+//documento procura um id que tem um elemento titulo, e altera o html para 
+document.querySelector('#titulo').innerHTML = 'Novo título'
